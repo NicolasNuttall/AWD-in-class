ac7db877c7c099040e874975171aa04e4bd651dd
NicolasNuttall <55877928+NicolasNuttall@users.noreply.github.com>
2020-12-15 23:21:05 +0000

    Login-Page

--- Gruntfile.js
+++ Gruntfile.js
@@ -55,8 +55,8 @@ module.exports = function(grunt){
                         "./node_modules/jquery/dist/jquery.min.js",
                         "./node_modules/jquery/dist/jquery.js",
                         "./node_modules/bootstrap/dist/js/bootstrap.min.js",                   
-                        "./node_modules/@glidejs/glide/dist/glide.min.js"
-                        
+                        "./node_modules/@glidejs/glide/dist/glide.min.js",
+                        "./node_modules/lightbox2/dist/js/lightbox.min.js"
                     ],
                 },
             },
